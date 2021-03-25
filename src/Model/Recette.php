03635e0da86ae8e49063f2326cbae5f433c7392b
Gregory <gregory.leblond.dev@gmail.com>
2021-03-25 15:02:09 +0100

    change list ignore

---
+++ src/Model/Recette.php
@@ -0,0 +1,92 @@
+<?php
+class Recette{
+    private $idrecipe;
+    private $recname;
+    private $recimg;
+    private $recidclient;
+    private $rechowmany;
+
+    /**
+     * @return mixed
+     */
+    public function getIdrecipe()
+    {
+        return $this->idrecipe;
+    }
+
+    /**
+     * @param mixed $idrecipe
+     */
+    public function setIdrecipe($idrecipe)
+    {
+        $this->idrecipe = $idrecipe;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRecname()
+    {
+        return $this->recname;
+    }
+
+    /**
+     * @param mixed $recname
+     */
+    public function setRecname($recname)
+    {
+        $this->recname = $recname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRecimg()
+    {
+        return $this->recimg;
+    }
+
+    /**
+     * @param mixed $recimg
+     */
+    public function setRecimg($recimg)
+    {
+        $this->recimg = $recimg;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRecidclient()
+    {
+        return $this->recidclient;
+    }
+
+    /**
+     * @param mixed $recidclient
+     */
+    public function setRecidclient($recidclient)
+    {
+        $this->recidclient = $recidclient;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRechowmany()
+    {
+        return $this->rechowmany;
+    }
+
+    /**
+     * @param mixed $rechowmany
+     */
+    public function setRechowmany($rechowmany)
+    {
+        $this->rechowmany = $rechowmany;
+    }
+
+    public function addRecipe(){
+        
+    }
+}
