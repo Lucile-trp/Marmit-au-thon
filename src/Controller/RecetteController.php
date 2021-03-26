1b3073ed21104ded96b4d9d8c53f59b04f4a4767
Gregory <gregory.leblond.dev@gmail.com>
2021-03-26 14:27:38 +0100

    Clean Code

--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -2,9 +2,18 @@
 namespace src\Controller;
 
 use src\Controller\AbstractController;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class RecetteController extends AbstractController {
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
     public function index(){
-        echo "page recette";
+        return $this->twig->render("Recette/index.html.twig");
     }
 }
