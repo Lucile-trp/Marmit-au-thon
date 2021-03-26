bc7b2d74a4f88fba9b004f090567f2c1428c4cab
mariamELALLALI <elallalimariam@gmail.com>
2021-03-26 16:35:28 +0100

    Ajout function new non fonctionnelle

--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -7,4 +7,23 @@ class RecetteController extends AbstractController {
     public function index(){
         echo "page recette";
     }
+
+    public function new(){
+      
+      /*  if(isset($_POST["recname"])){
+            //Traiter le formulaire = ajouter la recette en BDD
+            $recipe=new Recette;
+            $recipe->setTitre($_POST["recname"]);
+            $result=$recipe->SqlAdd(BDD::getInstance());
+            if($result =="ok"){
+                //Redirection
+            }else{
+                //Msg d'erreur
+            }
+        }else{
+
+        }
+        //Afficher le formulaire
+    }*/
+    }
 }
