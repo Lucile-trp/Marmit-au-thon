e48d5ce24920ba1f344d0927228376f82d439c91
Lucile TRIPIER <tripier.lucile@gmail.com>
2021-05-18 16:11:07 +0200

    add controller functions all/One

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,9 +1,35 @@
 <?php
 namespace src\Controller;
+use src\Model\Blog;
 
 class BlogController extends AbstractController
 {
     public function index(){
-        return $this->twig->render("Blog/blog.html.twig");
+
+        $article = Blog::getAll();
+        return $this->twig->render("Blog/blog.html.twig", [
+            "articles" => $article
+        ]);
+    }
+
+    //Fonction de redirection à la page du formulaire
+    //ON DOIT AVOIR LE ROLE ADMIN
+    public function addForm(){
+        return $this->twig->render("Blog/Add.html.twig");
+
+    }
+
+    //Fonction d'ajout d'un article
+    public function add(){
+
+    }
+
+    //affiche les détails d'un article
+    public function detail($id){
+        $article = Blog::getOne($id);
+        return $this->twig->render("Blog/detail.html.twig",[
+                "article" => $article
+            ]);
+
     }
 }
--- src/Model/Blog.php
+++ src/Model/Blog.php
@@ -1,7 +1,38 @@
 <?php
 
 namespace src\Model;
+use src\Model\BDD;
+use PDO;
 
 class Blog {
+    private $id;
+    private $title;
+    private $content;
+    private $date;
+    private $slug;
+    private $cli_id;
+
+    public function getAll(){
+        $bdd = BDD::getInstance();
+        $request = "SELECT * FROM article;";
+
+        $req = $bdd->prepare($request);
+        $req->execute();
+        $result = $req->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+
+    }
+
+    public static function getOne($id){
+        $bdd = BDD::getInstance();
+        $request = "SELECT * FROM article WHERE art_id=:id;";
+
+        $req = $bdd->prepare($request);
+        $req->bindParam(':id', $id);
+        $req->execute();
+        $result = $req->fetch();
+        return $result;
+
+    }
 
 }
