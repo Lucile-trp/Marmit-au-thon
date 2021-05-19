ed5500b110fea7cfcb6795fff2b573919dc7a7ee
Lucile TRIPIER <tripier.lucile@gmail.com>
2021-05-19 16:32:28 +0200

    feat add article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -19,10 +19,6 @@ class BlogController extends AbstractController
 
     }
 
-    //Fonction d'ajout d'un article
-    public function add(){
-
-    }
 
     //affiche les détails d'un article
     public function detail($id){
@@ -30,6 +26,24 @@ class BlogController extends AbstractController
         return $this->twig->render("Blog/detail.html.twig",[
                 "article" => $article
             ]);
+
+    }
+
+    //Fonction d'ajout d'un article en BDD
+    public function addArticle(){
+        $datas = $_POST;
+
+        if($datas['art-title'] != "" && $datas['art-resume'] != "" && $datas['art-content'] != ""){
+            $result = Blog::insertArticle($datas);
+            var_dump($result);
+
+        } else {
+            $error = "Il y a une erreur dans les informations";
+            return $this->twig->render("Blog/Add.html.twig",[
+                "error_message" => $error,
+            ]);
+
+        }
 
     }
 }
--- src/Model/Blog.php
+++ src/Model/Blog.php
@@ -3,6 +3,7 @@
 namespace src\Model;
 use src\Model\BDD;
 use PDO;
+use Cocur\Slugify\Slugify;
 
 class Blog {
     private $id;
@@ -32,6 +33,39 @@ class Blog {
         $req->execute();
         $result = $req->fetch();
         return $result;
+
+    }
+
+
+
+    public function insertArticle($datas){
+
+
+        $title = $datas['art-title'];
+        $resume = $datas['art-resume'];
+        $content = $datas['art-content'];
+
+        $slugify = new Slugify();
+        $slug = $slugify->slugify($title);
+
+        $cli_id = 1;
+
+        $bdd = BDD::getInstance();
+        $request = "INSERT INTO article(art_title, art_resume, art_content, art_slug, cli_id) VALUES (:title, :resume, :content, :slug, :cli_id);";
+        try {
+            $req = $bdd->prepare($request);
+            $req->bindParam(':title', $title);
+            $req->bindParam(':resume', $resume);
+            $req->bindParam(':content', $content);
+            $req->bindParam(':slug', $slug);
+            $req->bindParam(':cli_id', $cli_id);
+            $req->execute();
+
+
+    } catch(\PDOException $exception) {
+            echo $exception->getMessage();
+
+    }
 
     }
 
